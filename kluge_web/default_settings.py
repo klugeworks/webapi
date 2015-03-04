40cf7be538d1c1081ea78b58ebbc57992fc7b15e
Paul DiOrio <none>
2015-03-04 12:18:23 -0800

    Cleaned up default settings

--- kluge_web/default_settings.py
+++ kluge_web/default_settings.py
@@ -2,15 +2,13 @@ class Config(object):
     DEBUG = False
     TESTING = False
     KLUGE_WEB_DATASTORE = 'KlugeRedis'
-    #KLUGE_DS_HOSTNAME = 'redis.marathon.mesos'
-    KLUGE_DS_HOSTNAME = '10.30.3.24'
+    KLUGE_DS_HOSTNAME = 'redis.marathon.mesos'
     KLUGE_DS_PORT = 6379
 
 
 class ProductionConfig(Config):
     KLUGE_WEB_DATASTORE = 'KlugeRedis'
-    #KLUGE_DS_HOSTNAME = 'redis.marathon.mesos'
-    KLUGE_DS_HOSTNAME = '10.30.3.24'
+    KLUGE_DS_HOSTNAME = 'redis.marathon.mesos'
     KLUGE_DS_PORT = 6379
 
 
