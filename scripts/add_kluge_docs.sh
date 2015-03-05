ff4ae2188df7a941f6c280ef72304d0cf9c77a73
Paul DiOrio <none>
2015-03-04 19:51:28 -0800

    Modified runtime submission script to take api server as parameter

--- scripts/add_kluge_docs.sh
+++ scripts/add_kluge_docs.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 audio_split_dir=$1
-
+kluge_server=$2
 podcast_name=$(basename $audio_split_dir)
 
 chunkid=1
 for audio in $(ls $audio_split_dir/*.ul | sort); do
     basen=$(basename $audio);
     echo "$basen:$chunkid";
-    curl -F "docid=$podcast_name" -F "chunkid=$chunkid" -F "name=$basen" -F "bytes=@$audio" http://127.0.0.1:5000/jobs/english
+    curl -F "docid=$podcast_name" -F "chunkid=$chunkid" -F "name=$basen" -F "bytes=@$audio" http://$kluge_server/jobs/english
     chunkid=$((chunkid + 1));
 done
